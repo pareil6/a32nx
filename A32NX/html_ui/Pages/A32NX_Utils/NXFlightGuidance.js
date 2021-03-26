1639b10a91ee0bf7654c9128720584dad67b6f93
pareil6 <73158172+pareil6@users.noreply.github.com>
2021-03-26 08:00:27 +0000

    feat(ap): smooth custom AP guidance

--- A32NX/html_ui/Pages/A32NX_Utils/NXFlightGuidance.js
+++ A32NX/html_ui/Pages/A32NX_Utils/NXFlightGuidance.js
@@ -13,19 +13,33 @@ class NXFlightGuidance {
         this.nextToWaypoint = null;
         this.crossTrackError = null;
         this.trackAngleError = null;
+        this.turnCenter = null;
+        this.turnRadius = null;
+        this.turnActive = false;
     }
 
     update(_deltaTime) {
         const updatedWaypoints = this.updateWaypoints();
         if (updatedWaypoints) {
             this.updateLegs();
+
+            // If the errors were low before, we should be tracking correctly,
+            // so smooth the turn to the new track.
+            if (Math.abs(this.crossTrackError) < 0.05 && Math.abs(this.trackAngleError) < 5) {
+                this.turnCenter = null;
+                this.turnRadius = null;
+                this.turnActive = true;
+            }
+
             //console.log(`WPs: (${this.lastWaypoint ? this.lastWaypoint.ident : "null"}) - ${this.fromWaypoint ? this.fromWaypoint.ident : null} - ${this.toWaypoint ? this.toWaypoint.ident : null} - (${this.nextToWaypoint ? this.nextToWaypoint.ident : null})`);
             //console.log(`Tracks: (${Math.round(lastLegTrack)}) - ${Math.round(activeLegTrack)} - (${Math.round(nextLegTrack)})`);
         }
         this.updateErrors();
 
+        SimVar.SetSimVarValue("L:A32NX_FG_AVAIL", "bool", true);
         SimVar.SetSimVarValue("L:A32NX_FG_CROSS_TRACK_ERROR", "nautical miles", this.crossTrackError || 0);
         SimVar.SetSimVarValue("L:A32NX_FG_TRACK_ANGLE_ERROR", "degree", this.trackAngleError || 0);
+        SimVar.SetSimVarValue("L:A32NX_FG_PHI_COMMAND", "degree", 0);
     }
 
     updateWaypoints() {
@@ -86,6 +100,22 @@ class NXFlightGuidance {
         );
     }
 
+    _getPointOnTrack(from, dist, track) {
+        const earthRadius = 3440.1;
+        const deg2rad = Math.PI / 180;
+
+        const angDist = dist / earthRadius;
+        const trackRad = deg2rad * track;
+
+        const fromLat = deg2rad * from.lat;
+        const fromLon = deg2rad * from.long;
+
+        const lat = Math.asin(Math.sin(fromLat) * Math.cos(angDist) + Math.cos(fromLat) * Math.sin(angDist) * Math.cos(trackRad));
+        const lon = fromLon + Math.atan2(Math.sin(trackRad) * Math.sin(angDist) * Math.cos(fromLat), Math.cos(angDist) - Math.sin(fromLat) * Math.sin(lat));
+
+        return new LatLong(lat / deg2rad, lon / deg2rad);
+    }
+
     updateErrors(_deltaTime) {
         if (this.fromWaypoint === null || this.toWaypoint === null) {
             this.crossTrackError = null;
@@ -122,5 +152,48 @@ class NXFlightGuidance {
             Math.sin(deg2rad * (bearingAC - bearingAB))
         ) / deg2rad * earthRadius;
         this.crossTrackError = desiredOffset - actualOffset;
+
+        // If we have an active turn and the cross-track error is significant,
+        // use the turn center to calculate where we should be instead. Once
+        // the error becomes suitably small, we can disable the turn again and
+        // start using the leg directly (until the next leg).
+        if (Math.abs(this.crossTrackError) >= 0.05 && Math.abs(this.trackAngleError) >= 5) {
+            if (this.turnActive) {
+                // If we don't have the center yet, calculate it. At the point
+                // we start the turn, we want a circle arc from our current
+                // location to the point equidistant on the other side of the
+                // waypoint.
+                if (this.turnCenter === null) {
+                    const remDist = Avionics.Utils.computeGreatCircleDistance(ppos, this.fromWaypoint.infos.coordinates);
+                    const centerTrack = mod(trueTrack + 450, 360);
+
+                    // Radius is negative to the left, positive to the right,
+                    // matching the trackAngleError (positive means turn to the
+                    // right).
+                    this.turnRadius = remDist / Math.tan(deg2rad * this.trackAngleError / 2);
+
+                    // Fudge the centerpoint slightly closer, which has the
+                    // effect of making us turn marginally too tight, to
+                    // compensate for the AP lagging the guidance.
+                    this.turnRadius = this.turnRadius * 0.98;
+
+                    this.turnCenter = this._getPointOnTrack(ppos, this.turnRadius, centerTrack);
+                }
+
+                if (this.turnRadius > 0) { // Right turn
+                    this.trackAngleError = mod(Avionics.Utils.computeGreatCircleHeading(ppos, this.turnCenter) - trueTrack + 270 + 180, 360) - 180;
+                    this.crossTrackError = -this.turnRadius + Avionics.Utils.computeGreatCircleDistance(ppos, this.turnCenter);
+                } else { // Left turn
+                    this.trackAngleError = mod(Avionics.Utils.computeGreatCircleHeading(ppos, this.turnCenter) - trueTrack + 450 + 180, 360) - 180;
+                    this.crossTrackError = -this.turnRadius - Avionics.Utils.computeGreatCircleDistance(ppos, this.turnCenter);
+                }
+
+            }
+            console.log(ppos + " " + this.turnCenter + " " + this.turnRadius + " " + (this.turnCenter ? Avionics.Utils.computeGreatCircleDistance(ppos, this.turnCenter) : ""));
+        } else {
+            this.turnCenter = null;
+            this.turnRadius = null;
+            this.turnActive = false;
+        }
     }
 }
